4271d0199038d020f60cf5db1cf203265c2a3167
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2024-04-16 15:27:57 +0500

    DATABSE URL ADDITION

--- rentapp/main.py
+++ rentapp/main.py
@@ -1,4 +1,15 @@
 from sqlmodel import Field, SQLModel, create_engine
+from contextlib import asynccontextmanager
+from typing import Union, Optional, Annotated
+# from .settings import DATABASE_URL
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# print(DATABASE_URL)
 
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -13,5 +24,18 @@ class Rent(SQLModel, table=True):
     month: str
     status: str
     remarks: str | None = None
+
+
+# connection_string = str(DATABASE_URL).replace("postgresql", "postgresql+psycopg")
+# print(connection_string)
+
+engine = create_engine(DATABASE_URL, echo=True) # type: ignore
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    create_db_and_tables()   
+print(engine)
 
 
