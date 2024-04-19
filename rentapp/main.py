87b51bb81a0379982e30e26ebfa473b7aaff1648
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2024-04-19 08:52:46 +0500

    Data added in db

--- rentapp/main.py
+++ rentapp/main.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, Session,  SQLModel, create_engine
 from contextlib import asynccontextmanager
 from typing import Union, Optional, Annotated
 # from .settings import DATABASE_URL
@@ -6,11 +6,15 @@ from dotenv import load_dotenv
 import os
 
 
+# load environment variables from .env file
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# SQLITE_URL = os.getenv("SQLITE_URL")
 # print(DATABASE_URL)
 
+
+#class HERO with row attributes
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
@@ -34,8 +38,37 @@ engine = create_engine(DATABASE_URL, echo=True) # type: ignore
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-if __name__ == "__main__":
-    create_db_and_tables()   
-print(engine)
+def create_heroes():
+    hero1 = Hero(name="ShapatarMAN" , secret_name="SHP")
+    hero2 = Hero(name="ShapatarMAN2", secret_name="SHP2")
+    hero3 = Hero(name="ShapatarMAN3", secret_name="SHP3")
 
+    with Session(engine) as session:
+        session.add(hero1)
+        session.add(hero2)
+        session.add(hero3)
+
+        session.commit()
+
+def add_rent_details():
+    rent1 = Rent(description="Muzaffar Market", rent=9500, month="April 2024", status="received", remarks="Rent increased yearly" )
+    rent2 = Rent(description="Ramzan City", rent=10000, month="April 2024", status="not received")
+    rent3 = Rent(description="Ramzan Town", rent=10000, month="April 2024", status="Previous month due also")
+
+    with Session(engine) as session:
+        session.add(rent1)
+        session.add(rent2)
+        session.add(rent3)
+
+        session.commit()
+
+def main():
+    create_db_and_tables()
+    create_heroes()
+    add_rent_details()
+
+# Run if called main file, if imported does not call
+if __name__ == "__main__":
+    main()   
+# print(engine)
 
