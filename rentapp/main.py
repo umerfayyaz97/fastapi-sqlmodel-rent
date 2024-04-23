0284b25d86d74ed1832987f315ec94423c2335ab
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2024-04-23 23:44:50 +0500

    Reading data from DB

--- rentapp/main.py
+++ rentapp/main.py
@@ -1,4 +1,5 @@
 from sqlmodel import Field, Session,  SQLModel, create_engine
+from sqlmodel import select
 from contextlib import asynccontextmanager
 from typing import Union, Optional, Annotated
 # from .settings import DATABASE_URL
@@ -38,34 +39,49 @@ engine = create_engine(DATABASE_URL, echo=True) # type: ignore
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-def create_heroes():
-    hero1 = Hero(name="ShapatarMAN" , secret_name="SHP")
-    hero2 = Hero(name="ShapatarMAN2", secret_name="SHP2")
-    hero3 = Hero(name="ShapatarMAN3", secret_name="SHP3")
+# def create_heroes():
+#     hero1 = Hero(name="ShapatarMAN" , secret_name="SHP")
+#     hero2 = Hero(name="ShapatarMAN2", secret_name="SHP2")
+#     hero3 = Hero(name="ShapatarMAN3", secret_name="SHP3")
 
+#     with Session(engine) as session:
+#         session.add(hero1)
+#         session.add(hero2)
+#         session.add(hero3)
+
+#         session.commit()
+
+
+
+
+# def add_rent_details():
+#     rent1 = Rent(description="Muzaffar Market", rent=9500, month="April 2024", status="received", remarks="Rent increased yearly" )
+#     rent2 = Rent(description="Ramzan City", rent=10000, month="April 2024", status="not received")
+#     rent3 = Rent(description="Ramzan Town", rent=10000, month="April 2024", status="Previous month due also")
+
+#     with Session(engine) as session:
+#         session.add(rent1)
+#         session.add(rent2)
+#         session.add(rent3)
+
+#         session.commit()
+
+#getting data from tables
+def get_data():
     with Session(engine) as session:
-        session.add(hero1)
-        session.add(hero2)
-        session.add(hero3)
+        # statement = select(Hero)
+        # results = session.exec(statement)
+        # heroes = results.all()
+        # print(heroes)
 
-        session.commit()
-
-def add_rent_details():
-    rent1 = Rent(description="Muzaffar Market", rent=9500, month="April 2024", status="received", remarks="Rent increased yearly" )
-    rent2 = Rent(description="Ramzan City", rent=10000, month="April 2024", status="not received")
-    rent3 = Rent(description="Ramzan Town", rent=10000, month="April 2024", status="Previous month due also")
-
-    with Session(engine) as session:
-        session.add(rent1)
-        session.add(rent2)
-        session.add(rent3)
-
-        session.commit()
+        heroes = session.exec(select(Hero)).all()
+        print(heroes)
 
 def main():
     create_db_and_tables()
-    create_heroes()
-    add_rent_details()
+    # create_heroes()
+    # add_rent_details()
+    get_data()
 
 # Run if called main file, if imported does not call
 if __name__ == "__main__":
